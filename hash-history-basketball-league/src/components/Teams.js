a8834be916a47c1b40ce371a3b45566cf8e0df91
codejoncode <jonathanjholloway@gmail.com>
2018-08-10 20:03:47 -0500

    added the routes but no links yet. So to get to the players or teams page you have to type it in the browser

---
+++ hash-history-basketball-league/src/components/Teams.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+
+
+export default class Teams extends Component {
+
+    render(){
+        return(
+            <div className="container two-column">
+                TEAMS
+            </div>
+        )
+    }
+}
