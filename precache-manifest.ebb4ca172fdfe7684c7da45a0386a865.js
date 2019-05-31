219c16359bea59d36c8e5aa50db1d007fa0fd98d
codejoncode <jonathanjholloway@gmail.com>
2019-05-30 22:30:18 -0500

    Updates

---
+++ precache-manifest.ebb4ca172fdfe7684c7da45a0386a865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85abd0bad7470e47f7d2605e22046287",
+    "url": "/HashHistoryBasketballLeague/index.html"
+  },
+  {
+    "revision": "a27491c6f1048d146327",
+    "url": "/HashHistoryBasketballLeague/static/css/main.070773bf.chunk.css"
+  },
+  {
+    "revision": "d4223e552307954596b7",
+    "url": "/HashHistoryBasketballLeague/static/js/2.4a2d6637.chunk.js"
+  },
+  {
+    "revision": "a27491c6f1048d146327",
+    "url": "/HashHistoryBasketballLeague/static/js/main.f142c4ff.chunk.js"
+  },
+  {
+    "revision": "e6f0c563bd6a761c9318",
+    "url": "/HashHistoryBasketballLeague/static/js/runtime~main.b9c899bd.js"
+  }
+]);
